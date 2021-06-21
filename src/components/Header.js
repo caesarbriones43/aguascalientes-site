164b9c752aaffafd5449a8cd22055be9b0ea69d5
caesarbriones43 <caesarbriones43@gmail.com>
2021-06-20 23:08:26 -0500

    working on header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="header">
+          <h1>Aguascalientes</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
